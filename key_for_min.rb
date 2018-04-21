018880ab4914e598e8dc4db28b1286bb0a0310c1
Michael Boroski <mjboroski@gmail.com>
2018-04-21 05:06:12 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,7 +4,10 @@
 def key_for_min_value(name_hash)
   i=0
   name_hash.each do |name, value|
-    i==0||value<i ? i=value, j=name
+    if i==0||value<i
+      i=value
+      j=name
+    end
   end
   j
 end
